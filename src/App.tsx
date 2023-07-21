fab4bff591dba5e4b5d053ae6813d4f267e5593d
dikshant234 <dikshant.agarwal@rakuten.com>
2023-07-21 18:28:14 +0530

    chore:added tailwind css in todo component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Todo from './component/Todo';
 function App() {
 
   return (
-    <>
+    <div className='flex flex-col  bg-[#171723] w-[100%] h-[100vh] items-center justify-center'>
       <Todo />
-    </>
+    </div>
   )
 }
 
--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { useMainContext } from '../context'
+import { useMainContext } from '../context';
+import { AiOutlinePlusCircle } from "react-icons/Ai";
 
 export default function Todo() {
     const { text, setText, handleTodoInput, todo } = useMainContext();
     return (
-        <div className='flex'>
-            <div>Todo</div>
-            <input type='text' value={text} placeholder='Create a new Todo' onChange={(e) => setText(e.target.value)} />
-            <button onClick={handleTodoInput}>Submit</button>
+        <div className='flex flex-col bg-[#24273d] w-[500px] h-[700px] justify-center items-center'>
+            <h1>Todo</h1>
+            <div className='flex w-[500px] h-[5rem] justify-around items-center gap-20 p-10 flex-col bg-red-600' >
+                <button onClick={handleTodoInput} style={{ color: 'white' }}><AiOutlinePlusCircle /></button>
+                <input type='text' value={text} placeholder='Create a new Todo' onChange={(e) => setText(e.target.value)} />
+            </div>
+
             {todo.map((todo: string) => {
                 return <h2>{todo}</h2>
             })}
