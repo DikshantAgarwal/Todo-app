781500a9945a6992bc36f959458f73e7b9bdbcb8
Gunjan Aggarwal <dikshantagarwal234.com>
2023-07-25 16:54:05 +0530

    :fix:fix the Tailwind css issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Todo from './component/Todo';
 function App() {
 
   return (
-    <div className='flex flex-col  bg-[#171723] w-[100%] h-[100vh] items-center justify-center'>
+    <div className='flex flex-col  bg-gradient-to-t from-primary from-60% to-40% to-secondary w-[100%] h-[100vh] items-center justify-center'>
       <Todo />
     </div>
   )
--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -5,16 +5,22 @@ import { AiOutlinePlusCircle } from "react-icons/Ai";
 export default function Todo() {
     const { text, setText, handleTodoInput, todo } = useMainContext();
     return (
-        <div className='flex flex-col bg-[#24273d] w-[500px] h-[700px] justify-center items-center'>
-            <h1>Todo</h1>
-            <div className='flex w-[500px] h-[5rem] justify-around items-center gap-20 p-10 flex-col bg-red-600' >
-                <button onClick={handleTodoInput} style={{ color: 'white' }}><AiOutlinePlusCircle /></button>
-                <input type='text' value={text} placeholder='Create a new Todo' onChange={(e) => setText(e.target.value)} />
+        <>
+            <h1 className='text-white font-bold text-3xl'>TODO</h1>
+            <div className='flex flex-col  h-[500px]  mt-8'>
+                <div className='relative flex w-[500px]  bg-[#24273d]' >
+                    <button onClick={handleTodoInput} className='absolute text-white left-2.5 bottom-2.5'><AiOutlinePlusCircle size='30' /></button>
+                    <input className="shadow appearance-none border bg-[#24273d]  rounded w-full py-3 px-3 pl-20 h-14 text-white leading-tight focus:outline-none focus:shadow-outline" type='text' value={text} placeholder='Create a new Todo' onChange={(e) => setText(e.target.value)} />
+                </div>
+                <div className='flex flex-col w-[500px] bg-[#24273d] mt-16 divide-y rounded divide-white'>
+                    {todo.map((todo: string) => {
+                        return <div className='flex h-[60px] p-4'>
+                            <input type='radio' className='checked:bg-gray-900 checked:border-transparent mr-4 w-6 h-6' />
+                            <span className='text-white'> {todo}</span>
+                        </div>
+                    })}
+                </div>
             </div>
-
-            {todo.map((todo: string) => {
-                return <h2>{todo}</h2>
-            })}
-        </div>
+        </>
     )
 }
